63e0a13f3b19d0ea240082e9d7d043956e60209e
Sean Zellmer <sean@lejeunerenard.com>
2023-10-28 14:45:22 -0500

    feat: Add a test to account for scroll offset
    Clear the body since airtap adds the tap output to the body and there
    are default margins on the body.
    
    Set the background of both the fillSpaceElement & the tuioMap element to
    make it easy to visually debug if necessary.

--- tests/index.js
+++ tests/index.js
@@ -27,6 +27,11 @@ function onceEvent (event, eventCb) {
   ]
 }
 
+function clearBody () {
+  document.body.style.margin = '0'
+  ;[...document.body.children].forEach((c) => c.parentNode.removeChild(c))
+}
+
 test('TuioToTouch', (t) => {
   t.test('touch lifecycle', async (t) => {
     t.plan(7)
@@ -114,48 +119,122 @@ test('TuioToTouch', (t) => {
     await gotTouchEnd
   })
 
-  t.test('adjusts coordinates', async (t) => {
-    const width = 1000
-    const height = 750
+  t.test('adjusts coordinates', (t) => {
+    t.test('with offset element', async (t) => {
+      const width = 1000
+      const height = 750
 
-    const tuioX = 0.5
-    const tuioY = 0.1
+      const tuioX = 0.5
+      const tuioY = 0.1
 
-    const t2t = new TuioToTouch(dimensionsToFakeElement(width, height, { x: 123, y: 456 }))
+      const t2t = new TuioToTouch(dimensionsToFakeElement(width, height, { x: 123, y: 456 }))
 
-    // First touch
-    const [gotTouchStart] = onceEvent('touchstart', (event) => {
-      const { touches } = event
-      const touch = touches[0]
+      // First touch
+      const [gotTouchStart] = onceEvent('touchstart', (event) => {
+        const { touches } = event
+        const touch = touches[0]
 
-      t.equal(touch.screenX, width * tuioX + 123, 'screenX')
-      t.equal(touch.screenY, height * tuioY + 456, 'screenY')
-      t.equal(touch.clientX, width * tuioX + 123, 'clientX')
-      t.equal(touch.clientY, height * tuioY + 456, 'clientY')
-      t.equal(touch.pageX, width * tuioX + 123, 'pageX')
-      t.equal(touch.pageY, height * tuioY + 456, 'pageY')
-    })
+        t.equal(touch.screenX, width * tuioX + 123, 'screenX')
+        t.equal(touch.screenY, height * tuioY + 456, 'screenY')
+        t.equal(touch.clientX, width * tuioX + 123, 'clientX')
+        t.equal(touch.clientY, height * tuioY + 456, 'clientY')
+        t.equal(touch.pageX, width * tuioX + 123, 'pageX')
+        t.equal(touch.pageY, height * tuioY + 456, 'pageY')
+      })
 
-    t2t.parseTUIO({
-      elements: [
-        ['/tuio/2Dcur', 'source', 'TuioPad@10.0.0.1'],
-        ['/tuio/2Dcur', 'alive', 12],
-        [
-          '/tuio/2Dcur',
-          'set',
-          12,
-          tuioX,
-          tuioY,
-          0,
-          0,
-          0
+      t2t.parseTUIO({
+        elements: [
+          ['/tuio/2Dcur', 'source', 'TuioPad@10.0.0.1'],
+          ['/tuio/2Dcur', 'alive', 12],
+          [
+            '/tuio/2Dcur',
+            'set',
+            12,
+            tuioX,
+            tuioY,
+            0,
+            0,
+            0
+          ],
+          ['/tuio/2Dcur', 'fseq', 2390]
         ],
-        ['/tuio/2Dcur', 'fseq', 2390]
-      ],
-      oscType: 'bundle'
+        oscType: 'bundle'
+      })
+
+      await gotTouchStart
     })
 
-    await gotTouchStart
+    t.test('w/ viewport scroll', async (t) => {
+      const width = 1000
+      const height = 750
+      const offset = { x: 123, y: 456 }
+
+      const tuioX = 0.5
+      const tuioY = 0.1
+
+      const fillDimensions = {
+        width: window.innerWidth * 2,
+        height: window.innerHeight * 2
+      }
+
+      clearBody()
+
+      const fillSpaceElement = document.createElement('div')
+      fillSpaceElement.style.width = fillDimensions.width + 'px'
+      fillSpaceElement.style.height = fillDimensions.height + 'px'
+      fillSpaceElement.style.background = 'blue'
+
+      document.body.appendChild(fillSpaceElement)
+
+      const scrollPos = [fillDimensions.width / 2, fillDimensions.height / 2]
+      window.scrollTo(...scrollPos)
+
+      // Element TUIO Mapped to
+      const tuioMap = document.createElement('div')
+      tuioMap.style.width = width + 'px'
+      tuioMap.style.height = height + 'px'
+      tuioMap.style.position = 'absolute'
+      tuioMap.style.left = offset.x + 'px'
+      tuioMap.style.top = offset.y + 'px'
+      tuioMap.style.background = 'red'
+      document.body.appendChild(tuioMap)
+
+      const t2t = new TuioToTouch(tuioMap)
+
+      // First touch
+      const [gotTouchStart] = onceEvent('touchstart', (event) => {
+        const { touches } = event
+        const touch = touches[0]
+
+        t.equal(touch.screenX, width * tuioX + 123, 'screenX')
+        t.equal(touch.screenY, height * tuioY + 456, 'screenY')
+        t.equal(touch.clientX, width * tuioX + 123 - scrollPos[0], 'clientX')
+        t.equal(touch.clientY, height * tuioY + 456 - scrollPos[1], 'clientY')
+        t.equal(touch.pageX, width * tuioX + 123, 'pageX')
+        t.equal(touch.pageY, height * tuioY + 456, 'pageY')
+      })
+
+      t2t.parseTUIO({
+        elements: [
+          ['/tuio/2Dcur', 'source', 'TuioPad@10.0.0.1'],
+          ['/tuio/2Dcur', 'alive', 12],
+          [
+            '/tuio/2Dcur',
+            'set',
+            12,
+            tuioX,
+            tuioY,
+            0,
+            0,
+            0
+          ],
+          ['/tuio/2Dcur', 'fseq', 2390]
+        ],
+        oscType: 'bundle'
+      })
+
+      await gotTouchStart
+    })
   })
 
   t.test('skips old tuio events', async (t) => {
